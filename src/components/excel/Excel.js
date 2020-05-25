4d7c150f57d02dbd27445a86a3a9524f9b765e80
Alexey Muzichenko <almuz@inbox.lv>
2020-05-25 17:05:45 +0300

    Showing template from classes

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,3 +1,5 @@
+import { $ } from '@core/DOM'
+
 export class Excel {
 	constructor(selector, options) {
 		this.$el = document.querySelector(selector)
@@ -5,17 +7,26 @@ export class Excel {
 	}
 
 	getRoot() {
-		const $root = document.createElement('div')
+		// creating root element and
+		const $root = $.create('div', 'excel')
 
 		this.components.forEach((Component) => {
-			const component = new Component()
-			$root.insertAdjacentHTML('beforeend', component.toHTML())
+			// creating component element and adding to it class name from the CLASS
+			const $el = $.create('div', Component.className)
+
+			// init this Class
+			const component = new Component($el)
+
+			// getting components template
+			$el.innerHTML = component.toHTML()
+
+			// adding it to the root
+			$root.append($el)
 		})
 		return $root
 	}
 
 	render() {
-		console.log(this.$el)
 		this.$el.append(this.getRoot())
 	}
 }
--- src/core/DOMListener.js
+++ src/core/DOMListener.js
@@ -1,1 +1,9 @@
-export class DOMListener {}
+export class DOMListener {
+    constructor($root) {
+
+        // if (!$root) {
+        //     throw new Error('No $root provided to DomListener')
+        // }
+        this.$root = $root
+    }
+}
