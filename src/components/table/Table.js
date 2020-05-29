2e37397a398bf6f46b273ebabad1e1fc6d0f2dd9
Alexey Muzichenko <almuz@inbox.lv>
2020-05-29 16:54:22 +0300

    More listeners

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,7 +4,29 @@ import { createTable } from './table.template'
 export class Table extends ExcelComponent {
 	static className = 'excel__table'
 
+	constructor($root) {
+		super($root, {
+			listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+		})
+	}
+
 	toHTML() {
 		return createTable()
+	}
+
+	onClick(event) {
+		console.log('onClick', event.target);
+	}
+
+	onMousedown() {
+		console.log('onMousedown');
+	}
+
+	onMousemove() {
+		console.log('onMousemove');
+	}
+
+	onMouseup() {
+		console.log('onMouseup');
 	}
 }
