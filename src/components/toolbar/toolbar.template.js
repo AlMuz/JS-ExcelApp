7e3c0beff07a87acaf8d279d6fc237689cc7bc16
Alexey Muzichenko <almuz@inbox.lv>
2020-06-12 16:37:48 +0300

    More interactive toolbar buttons

--- src/components/toolbar/toolbar.template.js
+++ src/components/toolbar/toolbar.template.js
@@ -14,27 +14,35 @@ function createButton(button) {
 
 export function createToolbar(state) {
 	const buttons = [
-		{ icon: 'format_align_left', active: false, value: { textAlign: 'left' } },
+		{
+			icon: 'format_align_left',
+			active: state['textAlign'] === 'left',
+			value: { textAlign: state['textAlign'] !== 'left' ? 'left' : 'left'}
+		},
 		{
 			icon: 'format_align_center',
-			active: false,
-			value: { textAlign: 'center' }
+			active: state['textAlign'] === 'center',
+			value: { textAlign: state['textAlign'] === 'center' ? 'left' : 'center' }
 		},
 		{
 			icon: 'format_align_right',
-			active: false,
-			value: { textAlign: 'right' }
+			active: state['textAlign'] === 'right',
+			value: { textAlign: state['textAlign'] !== 'right' ? 'right' : 'left' }
 		},
 		{
 			icon: 'format_bold',
 			active: state['fontWeight'] === 'bold',
 			value: { fontWeight: state['fontWeight'] === 'bold' ? 'normal' : 'bold' }
 		},
-		{ icon: 'format_italic', active: false, value: { fontStyle: 'italic' } },
+		{
+			icon: 'format_italic',
+			active: state['fontStyle'] === 'italic',
+			value: { fontStyle: state['fontStyle'] === 'italic' ? 'normal' : 'italic' }
+		},
 		{
 			icon: 'format_underline',
-			active: false,
-			value: { textDecoration: 'underline' }
+			active: state['textDecoration'] === 'underline',
+			value: { textDecoration: state['textDecoration'] === 'underline' ? 'none' : 'underline' }
 		}
 	]
 	return buttons.map(createButton).join('')
