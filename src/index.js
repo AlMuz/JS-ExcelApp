f06d893dafd0b50757c1b8ac0cba292dacefedd0
Alexey Muzichenko <almuz@inbox.lv>
2020-06-13 15:53:53 +0300

    Added debounce to make smaller state dispatches

--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@ import { Toolbar } from '@/components/toolbar/Toolbar'
 import { Formula } from '@/components/formula/Formula'
 import { Table } from '@/components/table/Table'
 import { Store } from '@/redux/Store'
-import { storage } from '@core/utils'
+import { storage, debounce } from '@core/utils'
 import { rootReducer } from '@/redux/rootReducer'
 import { initialState } from '@/redux/initialState' 
 
 const storeClass = new Store(rootReducer, initialState)
 
-storeClass.subscribe((state) => {
+const stateListener = debounce((state) => {
 	console.log('app state', state)
 	storage('excel-state', state)
-})
+}, 300)
+
+storeClass.subscribe(stateListener)
 
 const excel = new Excel('#app', {
 	components: [Header, Toolbar, Formula, Table],
