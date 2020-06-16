52423c8f5d80b94ca6dd2b0848b44addb5ac9b77
Alexey Muzichenko <almuz@inbox.lv>
2020-06-16 17:00:37 +0300

    Realized header buttons

--- src/components/header/header.template.js
+++ src/components/header/header.template.js
@@ -5,12 +5,12 @@ export function createHeader(state) {
 	return `
         <input type="text" class="input" value="${title}" />
         <div class="buttons">
-            <div class="button">
-                <i class="material-icons"> delete </i>
+            <div class="button" data-button="remove">
+                <i class="material-icons" data-button="remove"> delete </i>
             </div>
-            <div class="button">
+            <a href="#" class="button">
                 <i class="material-icons"> exit_to_app </i>
-            </div>
+            </a>
         </div>
     `
 }
